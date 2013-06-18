670d8252125911171df05b03c81ccc74b6f0e191
Ivan Willig <iwillig@gmail.com>
2013-06-18 15:37:41 -0400

    remove qunit and a annotation file that was commit to the wrong branch

--- media/script/annotations.js
+++
@@ -1,8 +0,0 @@
-/*jslint browser: true, nomen: true, indent: 4, maxlen: 80 */
-/*global jQuery, OpenLayers */
-
-(function (global, OpenLayers, undefined) {
-    'use strict';
-
-
-}(window, OpenLayers));
