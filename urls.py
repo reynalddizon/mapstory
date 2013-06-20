35996d332aa56b8d97bd6a9344fb8a042652d76a
Ian Schneider <planablediglet@gmail.com>
2013-06-20 11:36:39 -0600

    remove superfluous static route

--- urls.py
+++ urls.py
@@ -159,9 +159,6 @@ if settings.SERVE_MEDIA:
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATIC_ROOT,
-        }),
         url(r'^thumbs/(?P<path>.*)$','django.views.static.serve',{
             'document_root' : settings.THUMBNAIL_STORAGE,
         })
