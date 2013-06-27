4fb9132187c18a5882389fc5050a89bae33f4ee6
Ian Schneider <planablediglet@gmail.com>
2013-06-27 15:52:16 -0600

    fix silly unicode output for ContactDetail

--- models.py
+++ models.py
@@ -293,6 +293,9 @@ class ContactDetail(Contact):
             name = self.user.username
         return name
 
+    def __unicode__(self):
+        return u"[ ContactDetail for %s ]" % self.display_name
+
 class ProfileIncomplete(models.Model):
     '''Track incomplete user profiles'''
     user = models.OneToOneField(User)
